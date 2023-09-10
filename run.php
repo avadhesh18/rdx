70fabbe5dfd6f92a7e87fe933619bc01b0b75f8b
avadhesh18 <avadhesh18@users.noreply.github.com>
2023-09-10 22:17:03 +0530

    fixed some thumbnail issues

--- run.php
+++ run.php
@@ -11,7 +11,7 @@ $headhtml = '<!DOCTYPE html><html><head>
      <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
     <meta charset="utf-8">
     <link rel="stylesheet" href="styles.css" type="text/css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/dashjs/4.7.1/dash.all.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dashjs/4.7.1/dash.all.min.js"></script>
 </head>   
 
 <body>
