de74e6cf173f73e0578f6ffeb9b3fb043ae3e405
avadhesh18 <avadhesh18@users.noreply.github.com>
2023-09-10 15:07:51 +0530

    fixed ios safari playback issue

--- public/functions.js
+++ public/functions.js
@@ -207,9 +207,10 @@ pjgd.items.forEach((item, index) => {
 	returnpost += '<div class="postc video">';
 		if(postjson['secure_media'] != null) {
 		vidurl =  postjson['secure_media']['reddit_video']['dash_url']; 
+		hlsurl =  postjson['secure_media']['reddit_video']['dash_url']; 
 		fallbackurl = postjson['secure_media']['reddit_video']['fallback_url']; 
 		//returnpost +='<video id="v'+postjson['id']+'" src="'+vidurl+'" poster="'+postjson["thumbnail"]+'" width="100%" height="240" preload="metadata" onplay="playaud(\'a'+postjson['id']+'\')"  onpause="pauseaud(\'a'+postjson['id']+'\')"  onseeking="pauseaud(\'a'+postjson['id']+'\')"  onseeked="seeked(\''+postjson['id']+'\')"   controls> </video><audio src="'+urli+'/DASH_audio.mp4" id="a'+postjson['id']+'" controls></audio>	';
-		returnpost +='<video id="v'+postjson['id']+'" src="'+vidurl+'" data-fallback="'+fallbackurl+'" poster="'+postjson["preview"]['images']['0']['source']['url']+'" width="100%" height="240" preload="metadata" class="reddit_hls"  controls> </video>';
+		returnpost +='<video id="v'+postjson['id']+'" src="'+vidurl+'" data-fallback="'+fallbackurl+'" data-hls="'+hlsurl+'" poster="'+postjson["preview"]['images']['0']['source']['url']+'" width="100%" height="240" preload="metadata" class="reddit_hls"  controls> </video>';
 		}
 		else {returnpost += 'crosspost';}
 	returnpost += '</div>';
@@ -339,9 +340,14 @@ function runhsl(){
             //    video.src = video.getAttribute('data-fallback');
                 
            // }
+           const isIOS = /iPhone|iPad/i.test(navigator.userAgent);
+if (isIOS) {
+ video.src = video.getAttribute('data-hls');
+}
+else {
            const hls = dashjs.MediaPlayer().create();
 hls.initialize(video,video.src, false);
-           
+         }
         });
 
 }
--- run.php
+++ run.php
@@ -7,8 +7,7 @@ $headhtml = ' <!DOCTYPE html><html><head>
       <link rel="manifest" href="/manifest.json" />
  <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-title" content="rdx" />
-    <meta   name="apple-mobile-web-app-status-bar-style"
-      content="default" />
+    <meta   name="apple-mobile-web-app-status-bar-style" content="default" />
      <meta name="viewport" content="width=device-width, initial-scale=1.0, 
 minimum-scale=1.0">
     <meta charset="utf-8">
