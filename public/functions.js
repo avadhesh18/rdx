6ef9fa07636a98ebb35855c689e518bfd68e3768
avadhesh18 <avadhesh18@users.noreply.github.com>
2023-09-21 14:01:35 +0530

    poster fixed

--- public/functions.js
+++ public/functions.js
@@ -218,7 +218,7 @@ let fakect = ' actv';
 		hlsurl =  postjson['secure_media']['reddit_video']['hls_url']; 
 		fallbackurl = postjson['secure_media']['reddit_video']['fallback_url']; 
 		//returnpost +='<video id="v'+postjson['id']+'" src="'+vidurl+'" poster="'+postjson["thumbnail"]+'" width="100%" height="240" preload="metadata" onplay="playaud(\'a'+postjson['id']+'\')"  onpause="pauseaud(\'a'+postjson['id']+'\')"  onseeking="pauseaud(\'a'+postjson['id']+'\')"  onseeked="seeked(\''+postjson['id']+'\')"   controls> </video><audio src="'+urli+'/DASH_audio.mp4" id="a'+postjson['id']+'" controls></audio>	';
-		vidposter = postjson["preview"]['images']['0']['source']['url'];
+		vidposter = postjson["preview"];
 		if(typeof vidposter == "undefined"){
 			vidposter = postjson["thumbnail"];
 		}
