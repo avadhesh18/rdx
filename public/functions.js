f20937bb6ff33746295d307f75b42613ba3246ec
avadhesh18 <avadhesh18@users.noreply.github.com>
2023-09-22 12:18:24 +0530

    fixed index issues

--- public/functions.js
+++ public/functions.js
@@ -385,7 +385,6 @@ document.getElementById("mi_"+curod).src= this.getAttribute('data-msrc');
   
   
   function shareit(){
-  	e.preventDefault();
   	if(navigator.share){const t=window.location.href;const n=document.title;const e={title:n,url:t};navigator.share(e).then(()=>{console.log("Shared successfully")}).catch(e=>{console.error("Error sharing:",e)})}
   	else{const t=window.location.href,n=document.createElement("textarea");n.value=t,document.body.appendChild(n),n.select(),document.execCommand("copy"),document.body.removeChild(n),alert("Link copied to clipboard: "+t)}
   }
